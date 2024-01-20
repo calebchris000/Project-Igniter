c799468fa6c1b407cc80159ea1311413bc462ae2
Caleb Nwaizu <calebchris000@gmail.com>
2024-01-20 22:37:14 +0100

    Add pending messages

--- src/app/v1/controller/message.js
+++ src/app/v1/controller/message.js
@@ -1,7 +1,8 @@
 const { HttpResponse } = require("../../../core/utils/Response");
 const uuid = require("uuid");
 const Message = require("../model/message");
-const websocket = require("../../../core/communication/chat/connection");
+const PendingMessage = require("../model/pendingMessage");
+const Account = require("../model/account");
 
 const GetMessages = async (req, res) => {
   try {
@@ -26,14 +27,60 @@ const SendMessage = async (req, res) => {
       status: "sent",
     });
 
-    websocket.on("open", () => {
-      websocket.send(JSON.stringify({ receiverId, content }));
-    });
-
     return HttpResponse(res, 201, "Message successfully sent", newMessage);
   } catch (error) {
     return HttpResponse(res, 500, error.toString());
   }
 };
 
-module.exports = { GetMessages, SendMessage };
+const SavePendingMessage = async (req, res) => {
+  try {
+    const { senderId, receiverId } = req.params;
+    const sender = await Account.findOne({ where: { id: senderId } });
+    const receiver = await Account.findOne({ where: { id: receiverId } });
+
+    if (!sender || !receiver) {
+      return HttpResponse(
+        res,
+        404,
+        `${!sender ? "Sender's" : "Receiver's"} account does not exist`
+      );
+    }
+
+    const newPendingMessage = await PendingMessage.create({ ...req.body });
+
+    return HttpResponse(
+      res,
+      201,
+      `${receiver.userName} would receive message when connected`,
+      newPendingMessage
+    );
+  } catch (error) {
+    return HttpResponse(res, 500, error.toString());
+  }
+};
+
+const DeletePendingMessages = async (req, res) => {
+  try {
+    const { receiverId } = req.params;
+
+    const receiver = await Account.findOne({ id: receiverId });
+
+    await PendingMessage.destroy({ where: { id: receiverId } });
+
+    return HttpResponse(
+      res,
+      200,
+      `All pending messages for recipient ${receiver.userName} are removed`
+    );
+  } catch (error) {
+    return HttpResponse(res, 500, error.toString());
+  }
+};
+
+module.exports = {
+  GetMessages,
+  SendMessage,
+  SavePendingMessage,
+  DeletePendingMessages,
+};
--- src/app/v1/router/message.js
+++ src/app/v1/router/message.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const verifyID = require("../../middlewares/verifyID");
-const { GetMessages, SendMessage } = require("../controller/message");
+const { GetMessages, SendMessage, SavePendingMessage, DeletePendingMessages } = require("../controller/message");
 
 const messageRouter = express.Router();
 
 messageRouter.get("/v1/user/messages/sender/:senderId/receiver/:receiverId/all", verifyID, GetMessages);
 messageRouter.post("/v1/user/messages/sender/:senderId/receiver/:receiverId/create", verifyID, SendMessage);
-
+messageRouter.post('/v1/user/messages/sender/:senderId/receiver/:receiverId/pending', verifyID, SavePendingMessage)
+messageRouter.delete('/v1/user/messages/receiver/:receiverId/pending', verifyID, DeletePendingMessages)
 module.exports = messageRouter;
